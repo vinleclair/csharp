c2c522dd1caca485d2c252bd01cd8b4a72187c8b
Vincent Leclair <vinleclair@gmail.com>
2019-05-06 16:29:43 -0400

    Add resistor color decoding

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private enum Color
+    {
+        black,
+        brown,
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        violet,
+        grey,
+        white
+    }
+    
+    public static int ColorCode(string color) 
+        => (int)Enum.Parse(typeof(Color), color); 
+
+    public static string[] Colors()
+        => Enum.GetNames(typeof(Color)); 
+}
