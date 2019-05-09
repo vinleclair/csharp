03300507ad77f5eb07f9991d665a7158a7f710fc
Vincent Leclair <vinleclair@gmail.com>
2019-05-09 14:42:27 -0400

    Add space age calculator

---
+++ space-age/SpaceAge.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class SpaceAge
+{
+    private double age;
+
+    public SpaceAge(double seconds)
+        => age = seconds;
+
+    public double OnEarth()
+        => age / 31557600;
+
+    public double OnMercury()
+        => OnEarth() / 0.2408467;
+
+    public double OnVenus()
+        => OnEarth() / 0.61519726;
+
+    public double OnMars()
+        => OnEarth() / 1.8808158;
+
+    public double OnJupiter()
+        => OnEarth() / 11.862615;
+
+    public double OnSaturn()
+        => OnEarth() / 29.447498;
+
+    public double OnUranus()
+        => OnEarth() / 84.016846;
+
+    public double OnNeptune()
+        => OnEarth() / 164.79132;
+}
